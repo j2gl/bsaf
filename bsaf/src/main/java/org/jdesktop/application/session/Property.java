a19b2d520d2b863cb68b7c78ecacd61285a28eca
etf <etf@no-email.com>
2009-10-17 11:33:26 +0000

    Session refactoring
    git-svn-id: https://svn.kenai.com/svn/bsaf~main/main@42 39130dca-0e0f-c9f4-f3e0-9e7262a0d940

---
+++ bsaf/src/main/java/org/jdesktop/application/session/Property.java
@@ -0,0 +1,45 @@
+package org.jdesktop.application.session;
+
+import java.awt.Component;
+
+/**
+ * Defines the {@code sessionState} property.  The value of this
+ * property is the GUI state that should be preserved across
+ * sessions for the specified component.  The type of sessionState
+ * values just one those supported by
+ * {@link java.beans.XMLEncoder XMLEncoder} and
+ * {@link java.beans.XMLDecoder XMLDecoder}, for example beans
+ * (null constructor, read/write properties), primitives, and
+ * Collections.
+ *
+ * @see #putProperty
+ * @see #getProperty(Class)
+ * @see #getProperty(Component)
+ */
+public interface Property {
+
+    /**
+     * Return the value of the {@code sessionState} property, typically
+     * a Java bean or a Collection the defines the {@code Component} state
+     * that should be preserved across Application sessions.  This
+     * value will be stored with {@link java.beans.XMLEncoder XMLEncoder},
+     * loaded with {@link java.beans.XMLDecoder XMLDecoder}, and
+     * passed to {@code setSessionState} to restore the Component's
+     * state.
+     *
+     * @param c the Component.
+     * @return the {@code sessionState} object for Component {@code c}.
+     * @see #setSessionState
+     */
+    Object getSessionState(Component c);
+
+    /**
+     * Restore Component {@code c's} {@code sessionState} from the specified
+     * object.
+     *
+     * @param c the Component.
+     * @param state the value of the {@code sessionState} property.
+     * @see #getSessionState
+     */
+    void setSessionState(Component c, Object state);
+}
