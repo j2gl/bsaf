e606a6d05026a691b9546eca7d9df3fe92c63b2f
robross <robross@no-email.com>
2009-10-29 01:00:25 +0000

    Removed dependency on assertions.jar and  javac2.jar
    git-svn-id: https://svn.kenai.com/svn/bsaf~main/branches/2.0@62 39130dca-0e0f-c9f4-f3e0-9e7262a0d940

--- bsaf/src/test/java/TestUpdateBranch2Only.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * @author Rob Ross
- * @version Date: Oct 28, 2009  5:14:15 PM
- */
-public class TestUpdateBranch2Only
-{
-    //dummy class to see if committing it it only writes to 2.0 branch
-}
