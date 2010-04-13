7d78d0c5456c090795c6e44da0201405f6b7f20f
etf <etf@no-email.com>
2010-04-13 21:10:35 +0000

    refs BSAF-77 Window Input Blocking with Delayed Progress Dialog improved RootPaneContainer discovery.
    git-svn-id: https://svn.kenai.com/svn/bsaf~main/trunk@127 39130dca-0e0f-c9f4-f3e0-9e7262a0d940

--- bsaf/src/main/java/org/jdesktop/application/utils/SwingHelper.java
+++ bsaf/src/main/java/org/jdesktop/application/utils/SwingHelper.java
@@ -5,6 +5,7 @@
 
 package org.jdesktop.application.utils;
 
+import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
@@ -15,8 +16,11 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Window;
 
+import javax.swing.JPopupMenu;
+import javax.swing.RootPaneContainer;
+
 /**
- *
+ * 
  * @author etf
  */
 public final class SwingHelper {
@@ -26,9 +30,9 @@ public final class SwingHelper {
 
     public static Rectangle computeVirtualGraphicsBounds() {
         Rectangle virtualBounds = new Rectangle();
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice[] gs =
-                ge.getScreenDevices();
+        GraphicsEnvironment ge = GraphicsEnvironment
+                .getLocalGraphicsEnvironment();
+        GraphicsDevice[] gs = ge.getScreenDevices();
         for (GraphicsDevice gd : gs) {
             GraphicsConfiguration gc = gd.getDefaultConfiguration();
             virtualBounds = virtualBounds.union(gc.getBounds());
@@ -45,14 +49,43 @@ public final class SwingHelper {
         }
         return resizable;
     }
-
+    /**
+     * Calculates default location for the specified window.
+     * @return default location for the window
+     * @param window the window location is calculated for.
+     *               It should not be null.
+     * @since 1.9
+     */
     public static Point defaultLocation(Window window) {
-        GraphicsConfiguration gc =
-                window.getGraphicsConfiguration();
+        GraphicsConfiguration gc = window.getGraphicsConfiguration();
         Rectangle bounds = gc.getBounds();
         Insets insets = window.getToolkit().getScreenInsets(gc);
         int x = bounds.x + insets.left;
         int y = bounds.y + insets.top;
         return new Point(x, y);
+    }
+
+    /**
+     * Finds the nearest RootPaneContainer of the provided Component. 
+     * Primarily, if a JPopupMenu (such as used by JMenus when they are visible) has no parent,
+     * the search continues with the JPopupMenu's invoker instead. Fixes BSAF-77
+     * 
+     * @return a RootPaneContainer for the provided component
+     * @param root the Component
+     * 
+     * @author Eric Heumann
+     * @since 1.9
+     */
+    public static RootPaneContainer findRootPaneContainer(Component root) {
+        while (root != null) {
+            if (root instanceof RootPaneContainer) {
+                return (RootPaneContainer) root;
+            } else if (root instanceof JPopupMenu && root.getParent() == null) {
+                root = ((JPopupMenu) root).getInvoker();
+            } else {
+                root = root.getParent();
+            }
+        }
+        return null;
     }
 }
