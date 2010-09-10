dc1f0ecd6e4774a11a8eb86887f0eb1fea70802e
etf <etf@no-email.com>
2010-09-10 09:41:42 +0000

    Mac Os X (Quit)
    git-svn-id: https://svn.kenai.com/svn/bsaf~main/framework/trunk@164 39130dca-0e0f-c9f4-f3e0-9e7262a0d940

--- src/main/java/org/jdesktop/application/Application.java
+++ src/main/java/org/jdesktop/application/Application.java
@@ -16,6 +16,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.PaintEvent;
 import java.beans.Beans;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.EventListener;
 import java.util.EventObject;
 import java.util.List;
